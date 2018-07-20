4deb82edaa4a88ba17f992ecbce63f160d1dcff4
Black-Seraph <admin@black-seraph.com>
2018-07-20 13:51:10 +0200

    xds: Fixed the copy-paths

--- xds/xds-vendor.mk
+++ xds/xds-vendor.mk
@@ -1,14 +1,14 @@
 #lib
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gpd/xds/proprietary/lib,system/lib)
 #lib64
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/lib64,system/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gpd/xds/proprietary/lib64,system/lib64)
 #bin
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gpd/xds/proprietary/bin,system/bin)
 #etc
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gpd/xds/proprietary/etc,system/etc)
 #usr
-#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/usr,system/usr)
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gpd/xds/proprietary/usr,system/usr)
 #xbin
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gpd/xds/proprietary/xbin,system/xbin)
 #vendor
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gpd/xds/proprietary/vendor,system/vendor)
